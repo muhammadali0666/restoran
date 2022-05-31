ed9d2c89ad348598b39113a365600eeaffdb797a
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2022-05-31 16:14:07 +0500

    init

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+
+const advice = Number(prompt("xarajatni kriting!"))
+
+if ( advice >= 50 || advice <= 300) {
+  console.log("15% to'lash kerak.");
+} else if (advice > 300) {
+  console.log("20% to'lash kerak");
+}
